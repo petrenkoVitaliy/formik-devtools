8a6f19c7253585f506776093cbf1def16b1a9a54
petrenkoVitaliy <vitaliy.petrenko@923digital.com>
2020-10-19 01:05:39 +0300

    changed api functionality

--- public/scripts/content.js
+++ public/scripts/content.js
@@ -6,19 +6,49 @@
         s.setAttribute('type', 'text/javascript');
         s.setAttribute('src', file);
         document.documentElement.appendChild(s);
+        console.log('content injected');
+    }
+    injectScript(chrome.extension.getURL('/scripts/war.js'));
+
+    let messagesList = [];
+
+    document.addEventListener('formikDevtoolsEvent', ({ detail: formikProps }) => {
+        messagesList.push({ formikProps });
+        tryToConnect();
+    });
+
+    const runtimeId = chrome.runtime.id;
+    let port;
+    let isConnected = false;
+
+    function tryToConnect() {
+        if (!isConnected) {
+            try {
+                port = chrome.runtime.connect(runtimeId);
+                isConnected = true;
+            } catch (ex) {
+                isConnected = false;
+                console.log(ex);
+            }
+            if (isConnected) {
+                sendMessages();
+            }
+        } else {
+            sendMessages();
+        }
+    }
+
+    function sendMessages() {
+        messagesList.forEach((message) => {
+            port.postMessage(message);
+        });
+        messagesList = [];
     }
 
     chrome.runtime.onMessage.addListener(function (request) {
         if (request.message === 'start') {
-            const runtimeId = chrome.runtime.id;
-            const port = chrome.runtime.connect(runtimeId);
-
-            injectScript(chrome.extension.getURL('/scripts/war.js'));
-
-            console.log('content injected');
-            document.addEventListener('formikDevtoolsEvent', ({ detail: formikProps }) => {
-                port.postMessage({ formikProps });
-            });
+            port = chrome.runtime.connect(runtimeId);
+            isConnected = true;
         }
     });
 })();
